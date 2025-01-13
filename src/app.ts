3c4940d59b4faec9e64da48f49c72e89e603a054
Mitsuyuki Shiiba <bufferings@gmail.com>
2025-01-13 14:34:15 +0900

    Split user handler

--- src/app.ts
+++ src/app.ts
@@ -1,96 +1,8 @@
 import { swaggerUI } from '@hono/swagger-ui';
-import { OpenAPIHono, createRoute, z } from '@hono/zod-openapi';
-
-const ParamsSchema = z.object({
-  id: z
-    .string()
-    .min(3)
-    .openapi({
-      param: {
-        name: 'id',
-        in: 'path',
-      },
-      example: '1212121',
-    }),
-});
-
-const UserSchema = z
-  .object({
-    id: z.string().openapi({
-      example: '123',
-    }),
-    name: z.string().openapi({
-      example: 'John Doe',
-    }),
-    age: z.number().openapi({
-      example: 42,
-    }),
-  })
-  .openapi('User');
-
-const ErrorSchema = z.object({
-  code: z.number().openapi({
-    example: 400,
-  }),
-  message: z.string().openapi({
-    example: 'Bad Request',
-  }),
-});
-
-const route = createRoute({
-  method: 'get',
-  path: '/users/{id}',
-  request: {
-    params: ParamsSchema,
-  },
-  responses: {
-    200: {
-      content: {
-        'application/json': {
-          schema: UserSchema,
-        },
-      },
-      description: 'Retrieve the user',
-    },
-    400: {
-      content: {
-        'application/json': {
-          schema: ErrorSchema,
-        },
-      },
-      description: 'Returns an error',
-    },
-  },
-});
+import { OpenAPIHono } from '@hono/zod-openapi';
+import { registerUserHandler } from './users/user_handler.js';
 
 const app = new OpenAPIHono();
-
-app.openapi(
-  route,
-  (c) => {
-    const { id } = c.req.valid('param');
-    return c.json(
-      {
-        id,
-        age: 20,
-        name: 'Ultra-man',
-      },
-      200, // You should specify the status code even if it is 200.
-    );
-  },
-  // Hook
-  (result, c) => {
-    if (!result.success) {
-      return c.json(
-        {
-          code: 400,
-          message: 'Validation Error',
-        },
-        400,
-      );
-    }
-  },
-);
 
 // The OpenAPI documentation will be available at /doc
 app.doc('/doc', {
@@ -103,5 +15,7 @@ app.doc('/doc', {
 
 // Use the middleware to serve Swagger UI at /ui
 app.get('/ui', swaggerUI({ url: '/doc' }));
+
+registerUserHandler(app);
 
 export default app;
